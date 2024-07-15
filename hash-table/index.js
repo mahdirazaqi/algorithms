3d1f59c0c7913c38b0bfe078b6e92ab4902c8455
MahdiRazaqi <MahdiRazaqi77@gmail.com>
2024-07-15 19:44:42 +0330

    hash table

---
+++ hash-table/index.js
@@ -0,0 +1,10 @@
+const hashTable = new Map();
+
+hashTable.set("apple", "1$");
+
+hashTable.set("apple", "2$");
+
+hashTable.set("milk", "3$");
+
+console.log(hashTable.get("milk"));
+console.log(hashTable);
